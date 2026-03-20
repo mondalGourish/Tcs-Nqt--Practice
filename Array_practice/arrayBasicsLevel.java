2a8c90aaff789c934fa011536d54f501c5e69cc8
mondalGourish <gourishmondal02@gmail.com>
2026-03-20 19:34:36 +0530

    new

--- Array_practice/arrayBasicsLevel.java
+++ Array_practice/arrayBasicsLevel.java
@@ -120,3 +120,35 @@
     
 //   }
 // }
+
+//count the frequency of elements
+// import java.util.*;
+// public class arrayBasicsLevel{
+//   public static void countFreq(int arr[]){
+//     boolean visited[] = new boolean[arr.length];
+//     for(int i=0; i<arr.length; i++){
+//       if(visited[i]){
+//         continue;
+//       }
+//       int count = 1;
+//       for(int j=i+1; j<arr.length; j++){
+//         if(arr[i] == arr[j]){
+//           visited[j] = true;
+//           count++;
+//         }
+//       }
+//       System.out.println(arr[i]+" "+count);
+//     }
+    
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int arr[] = new int[n];
+//     for(int i=0; i<arr.length; i++){
+//       arr[i] = sc.nextInt();
+//     }
+//     countFreq(arr);
+//   }
+// }
+
