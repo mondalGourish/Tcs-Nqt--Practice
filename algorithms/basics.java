c8910b5a56177da0b5583d2aa4058d2a9d9f070e
mondalGourish <gourishmondal02@gmail.com>
2026-03-23 22:09:47 +0530

    new codes

--- algorithms/basics.java
+++ algorithms/basics.java
@@ -52,3 +52,70 @@
 
 //   }
 // }
+
+//bubble sort
+//bubble sort
+// import java.util.*;
+// public class basics{
+//   public static void bubbleSort(int arr[]){
+//     int n = arr.length;
+//     for(int i=0; i<n-1; i++){
+//       int swap = 0;
+//       for(int j=0; j<n-1-i; j++){
+//         if(arr[j]>arr[j+1]){
+//           int temp = arr[j];
+//           arr[j] = arr[j+1];
+//           arr[j+1] = temp;
+//           swap++;
+//         }
+//       }
+//       if(swap == 0){
+//         break;
+//       }
+//     }
+    
+//     for(int i=0; i<n; i++){
+//       System.out.print(arr[i]+" ");
+//     }
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int arr[] = new int[n];
+//     for(int i=0; i<n; i++){
+//       arr[i] = sc.nextInt();
+//     }
+//     bubbleSort(arr);
+//   }
+// }
+
+//selection sort
+// import java.util.*;
+// public class basics{
+//   public static void selectionSort(int arr[]){
+//     int n = arr.length;
+//     for(int i=0; i<n-1; i++){
+//       int minPos = i;
+//       for(int j=i+1; j<n; j++){
+//         if(arr[minPos]>arr[j]){
+//           minPos = j;
+//         }
+//       }
+//       int temp = arr[minPos];
+//       arr[minPos] = arr[i];
+//       arr[i] = temp;
+//     }
+//     for(int i=0; i<n; i++){
+//       System.out.print(arr[i]+" ");
+//     }
+//   }
+//   public static void main(String args[]){
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int arr[] = new int[n];
+//     for(int i=0; i<n; i++){
+//       arr[i] = sc.nextInt();
+//     }
+//     selectionSort(arr);
+//   }
+// }
